e08815a02d77b30ca14b357a69212a8ae9c708b7
Aditya Panda <adtyapnda@gmail.com>
2025-07-06 01:41:32 +0530

    Update ClarityAI.js

--- src/components/ClarityAI.js
+++ src/components/ClarityAI.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
+import LanguageSwitcher from './LanguageSwitcher';
 
 const ClarityAI = () => {
   const [currentScreen, setCurrentScreen] = useState('intro');
